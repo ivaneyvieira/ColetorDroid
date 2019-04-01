5784cea2eea40867776d3f22eddadc9a8892ea8f
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2019-04-01 07:47:17 -0300

    Interface UI pronta

--- app/src/main/java/br/com/pintos/coletordroid/MainActivity.kt
+++ app/src/main/java/br/com/pintos/coletordroid/MainActivity.kt
@@ -2,11 +2,17 @@ package br.com.pintos.coletordroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val text = edtLeitura
+
     }
+
+
 }
